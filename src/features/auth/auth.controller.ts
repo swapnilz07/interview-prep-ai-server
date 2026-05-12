910d512d6ee9085d7f2e5673d7d5dc111e0f16c6
swapnil <swapnil.zakade.33@gmail.com>
2026-05-13 03:41:11 +0530

    updated cookie part.

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -100,7 +100,8 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: env.NODE_ENV === "production",
-      sameSite: "strict",
+      // sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
